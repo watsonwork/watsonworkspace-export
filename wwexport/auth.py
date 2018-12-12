658c0c0060c3baf13bfd2a1fb9624940bc5f161d
Jon Brunn <jbrunn@us.ibm.com>
2018-12-12 10:38:48 -0700

    change to absolute imports for bundling tool compatibility

--- wwexport/auth.py
+++ wwexport/auth.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import constants
+from wwexport import constants
 
 import sys
 import requests
--- wwexport/core.py
+++ wwexport/core.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import queries
-import auth
+from wwexport import queries
+from wwexport import auth
+from wwexport import constants
 
-import constants
 import logging
 import csv
 import datetime
